1c55216cbb95014c7489abc3e083549f32217fe5
Nick Ooms <oomsni@gmail.com>
2017-08-22 08:27:08 +0200

    replaced double by single quotes

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -45,7 +45,7 @@ export const resolvers = {
     },
     addMessage: (root, { message }) => {
       const channel = channels.find(channel => channel.id === message.channelId);
-      if (!channel) throw new Error("Channel does not exist");
+      if (!channel) throw new Error('Channel does not exist');
       const newMessage = { id: String(nextMessageId++), text: message.text };
       channel.messages.push(newMessage);
       pubsub.publish('messageAdded', { messageAdded: newMessage, channelId: message.channelId });
