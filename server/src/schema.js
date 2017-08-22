4a5312483841eabfac72b2e3bed6dbc919720b92
Nick Ooms <oomsni@gmail.com>
2017-08-22 16:03:06 +0200

    split up typeDefs

--- server/src/schema.js
+++ server/src/schema.js
@@ -2,37 +2,49 @@ import { makeExecutableSchema } from 'graphql-tools';
 
 import { resolvers } from './resolvers';
 
-const typeDefs = `
+const Channel = `
 type Channel {
   id: ID!
   name: String
   messages: [Message]!
 }
+`;
 
+const MessageInput = `
 input MessageInput{
   channelId: ID!
   text: String
 }
+`;
 
+const Message = `
 type Message {
   id: ID!
   text: String
 }
+`;
 
+const Query = `
 type Query {
   channels: [Channel]
   channel(id: ID!): Channel
 }
+`;
 
+const Mutation = `
 type Mutation {
   addChannel(name: String!): Channel
   addMessage(message: MessageInput!): Message
 }
+`;
 
+const Subscription = `
 type Subscription {
   messageAdded(channelId: ID!): Message
 }
 `;
+
+const typeDefs = [Channel, MessageInput, Message, Query, Mutation, Subscription];
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
